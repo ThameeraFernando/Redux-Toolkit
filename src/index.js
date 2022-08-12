316c8e96d7761b63438bae07c6651522cf6769cb
ThameeraFernando <sithnuwan111@gmail.com>
2022-08-12 10:18:10 +0530

    create navBar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import NavBar from "./components/NavBar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <NavBar />
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
