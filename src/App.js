50c830c2be0ed41b84bf8526f6ae554d55d7597c
ThameeraFernando <sithnuwan111@gmail.com>
2022-08-12 13:33:21 +0530

    use async thunk to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import CartContainer from "./components/CartContainer";
 import NavBar from "./components/NavBar";
 import { useEffect } from "react";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "./components/Modal";
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   useEffect(() => {
+    console.log(calculateTotals());
+    dispatch(getCartItems("random"));
+  }, []);
+  useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <main>
       {isOpen && <Modal />}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,37 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { openModal } from "../modal/modalSlice";
+// import cartItems from "../../cartItems";
+const url = "https://course-api.com/react-useReducer-cart-project";
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
 };
+
+// export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+//   return fetch(url)
+//     .then((resp) => resp.json())
+//     .catch((error) => console.log(error));
+// });
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    try {
+      //   console.log(name);
+      //   console.log(thunkAPI);
+      //   console.log(thunkAPI.getState());
+      //   thunkAPI.dispatch(openModal());
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
+
 //immer library handle the states when we are changing the state
 const cartSlice = createSlice({
   name: "cart",
@@ -38,6 +63,20 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action.payload);
+      state.isLoading = false;
     },
   },
 });
